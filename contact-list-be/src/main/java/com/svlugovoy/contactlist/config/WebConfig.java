066ba9c48cf72169c4bd9cd0f4ba490277f2fe2e
svlugovoy <svlugovoy@gmail.com>
2019-05-11 22:21:19 +0300

    CL-005: Enabled global cors

---
+++ contact-list-be/src/main/java/com/svlugovoy/contactlist/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.svlugovoy.contactlist.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig  implements WebMvcConfigurer{
+
+    /**
+     * Enables Cross-Origin Resource Sharing (CORS)
+     * More info: http://docs.spring.io/spring/docs/current/spring-framework-reference/html/cors.html
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
