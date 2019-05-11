1cc2393199cf34eae6c17abe1fab43f05ef8c692
Sergey Lugovoy <sergey.lugovoy@resmed.com>
2019-05-11 10:57:10 +0300

    CL-001: Initial commit

---
+++ contact-list-be/src/main/java/com/svlugovoy/contactlist/ContactListBeApplication.java
@@ -0,0 +1,13 @@
+package com.svlugovoy.contactlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactListBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactListBeApplication.class, args);
+    }
+
+}
