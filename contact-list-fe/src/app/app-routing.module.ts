cbd115f54c0e8a8b9a6b85a8dd519f862f882de5
svlugovoy <svlugovoy@gmail.com>
2019-05-12 13:23:09 +0300

    CL-011: Started add/edit contact functional

--- contact-list-fe/src/app/app-routing.module.ts
+++ contact-list-fe/src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
 import {ContactListComponent} from "./components/contact-list/contact-list.component";
 import {ContactDetailsComponent} from "./components/contact-details/contact-details.component";
+import {ContactAddTdfComponent} from "./components/contact-add-tdf/contact-add-tdf.component";
+import {ContactEditTdfComponent} from "./components/contact-edit-tdf/contact-edit-tdf.component";
+import {ContactEditRfComponent} from "./components/contact-edit-rf/contact-edit-rf.component";
+import {ContactAddRfComponent} from "./components/contact-add-rf/contact-add-rf.component";
 
 const routes: Routes = [
   {
@@ -21,6 +25,22 @@ const routes: Routes = [
   {
     path: 'contact-details/:id',
     component: ContactDetailsComponent
+  },
+  {
+    path: 'add-contact-tdf',
+    component: ContactAddTdfComponent
+  },
+  {
+    path: 'edit-contact-tdf/:id',
+    component: ContactEditTdfComponent
+  },
+  {
+    path: 'add-contact-rf',
+    component: ContactAddRfComponent
+  },
+  {
+    path: 'edit-contact-rf/:id',
+    component: ContactEditRfComponent
   }
 ];
 
